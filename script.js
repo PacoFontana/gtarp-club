459dc2a8977f0de2e797d9c1f56da0f94102f5ce
PacoFontana <pacopc2020@gmail.com>
2024-04-18 18:07:27 -0300

    1.5

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+});
